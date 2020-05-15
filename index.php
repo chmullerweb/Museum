ffd1208686f9c4589eb24dd4bbd0f45dac8d3805
chmullerweb <ch.muller.web@gmail.com>
2020-05-15 22:37:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Museum - L'art en couleur</title>
+
+    <!-- ////   CSS   //// -->
+    <link rel="stylesheet" href="css/cssreset.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- ////   PATTERN   //// -->
+    <link href="https://unpkg.com/pattern.css" rel="stylesheet">
+
+    <!-- //// FONT ////-->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
+
+</head>
+
+<body class="grid">
+
+    <header>
+        <nav>
+            <ul class="flex">
+                <li class="white">Accueil</li>
+                <li class="white">Artistes</li>
+                <li class="white">Tendances</li>
+                <li class="white">Contact</li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="titre col6 pattern-dots-md red max-w-20pc overflow-visible">">
+        <h1 class="siteName white">Museum</h1>
+    </div>
+
+    <div class="col6 claim white">
+        <h2>L'art en couleurs</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum officiis neque nam cumque iste laborum consectetur delectus aspernatur id ducimus, possimus laudantium impedit. Velit reprehenderit consequatur nostrum officiis in perspiciatis.</p>
+    </div>
+
+    <div class="col3">
+        <img src="img/giulia-may-tz4dVl1Lk1o-unsplash.jpg" alt="" class="header-pix">
+    </div>
+    <div class="col3">
+        <img src="img/jannis-lucas-T4t9nr18DF8-unsplash.jpg" alt="" class="header-pix">
+    </div>
+    <div class="col3">
+        <img src="img/jay-chan-T5FRGJbBEQE-unsplash.jpg" alt="" class="header-pix">
+    </div>
+    <div class="col3">
+        <img src="img/wengang-zhai-Ts1W-Xbt_Qk-unsplash.jpg" alt="" class="header-pix">
+    </div>
+
+    <!-- <div class="header-txt-bloc col12">
+        <h3 class="header-txt bg-grey white text-pattern pattern-dots-sm
+">Portrait Urbain</h3>
+        </div>-->
+
+    <div class="header-txt-bloc col12">
+        <h3 class="header-txt bg-grey">Portrait Urbain</h3>
+    </div>
+
+
+    <div class="white artist-txt col3 mg-l30">
+        <h2 class="orange">TokyoLuv</h2>
+        <p>Artiste du mois</p>
+        <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sit optio quasi iste aut natus quas temporibus tempore suscipit voluptatibus aperiam modi incidunt perspiciatis, rem sint culpa dolor molestiae, dignissimos!</p>
+    </div>
+
+    <div class=" expo-bloc row2 col-st5 col3">
+        <p class="expo-txt">Exposition en cours :<br>
+            <span class="bold">Lumières d'été - Simone Sunrise</span>
+        </p>
+        <p class="expo-txt">Laborum voluptas, cum est culpa consequatur in voluptatem ipsum, saepe non nemo consectetur odio fostrum nihil quae voluptate, nobis labore fugiat excepturi. Consectetur adipisicing elit.<br><br>
+            Natus consequuntur totam. Odit ipsumnihil quae voluptate, nobis dolor sit amet, consectetur adipisicing elit. Cum porro labore non laborum, dolorem voluptas, quasi aliquid quos, molestias voluptatibus consectetur. <br><br>
+            Consectetur adipisicing elit. Id nihil sed enim odit, porro maxime nobis temporibus impedit dicta? Quos ratione ea non unde officiis aut perspiciatis eveniet in quasi? </p><br>
+        <p class="expo-txt italic">
+            Beatae in dicta quaerat 2020.
+        </p>
+    </div>
+
+    <div class="col4 row2">
+        <img src="img/ian-dooley-ZLBzMGle-nE-unsplash.jpg" alt="" class="expo-pix">
+    </div>
+
+    <div class="col3 mg-l30">
+        <img src="img/tokyo-luv-CsMNgdHXzFs-unsplash.jpg" alt="" class="artist-pix">
+    </div>
+
+    <div class="musee-pix-bloc col12 pattern-dots-md orange max-w-20pc overflow-visible">
+        <img src="img/scott-webb-ab80p262fFM-unsplash.jpg" alt="" class="musee-pix">
+    </div>
+
+
+    <div class="row12 col3">
+        <nav>
+            <ul class="">
+                <li class="white"><a href="index.php">Accueil</a></li>
+                <li class="white"><a href="artist.php">Artistes</a></li>
+                <li class="white"><a href="trend.php">Tendances</a></li>
+                <li class="white"><a href="#"></a>Horaires et Tarifs</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="flex row12 col3">
+        <ul class="footer-list">
+            <li class="bold">Museum - Art en couleurs</li>
+            <br>
+            <li class="italic">124 Avenue des Beaux-Arts</li>
+            <li class="italic">75001 Paris</li>
+        </ul>
+    </div>
+    <div class="row12 col3">
+        <p class="cond-general">-Conditions générales-</p>
+    </div>
+    <div class="row12 col3">
+        <p class="bold">Contact</p>
+        <p>
+        <a href="" tel="0109452300" class="contact-txt">O1.09.45.23.00</a><br>
+        <a href="" mail="contact@museumartcolor.com" class="contact-txt">contact@museumartcolor.com</a></p>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
---
+++ readMe.php
@@ -0,0 +1,76 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Jour 5</title>
+
+    <!-- ////   CSS   //// -->
+    <link rel="stylesheet" href="css/cssreset.css">
+    <link rel="stylesheet" href="css/styleReadMe.css">
+    
+    <!-- //// FONT ////-->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
+    
+</head>
+
+<body>
+
+    <h1>Mon (petit) musée</h1>
+
+    <div class="intro">
+        Je souhaite réaliser le site internet d'un musée. <br>
+        Dans un premier temps, il faut réaliser les pages HTML. <strong>Il n'y a aucun code PHP</strong>
+        <hr>
+
+        Sur toutes les pages :
+        <ul>
+            <li>Un bandeau de navigation (logo + navigation)</li>
+            <li>un footer avec la navigation + lien "conditions générales" + l'adresse du musée</li>
+        </ul>
+
+        Page d'accueil
+        <ul>
+            <li>une photo du musée et une description</li>
+            <li>une oeuvre (qui sera tiré au hasard dans la collection) et sa description</li>
+        </ul>
+
+        Page liste des artistes
+        <ul>
+            <li>
+                un moteur de recherche avec le choix du type d'artiste (photographe, peintre, graveur...) cette liste
+                pourra évoluer au fil du temps
+            </li>
+            <li>
+                Le résultat de la recherche :
+                1 ligne de résultat =
+                <ul>
+                    <li> Une photo de l'artiste</li>
+                    <li>résumé de la biographie de l'artiste </li>
+                </ul>
+            </li>
+        </ul>
+
+        Page détail d'un artiste
+        <ul>
+            <li>la biographie complète de l'artiste</li>
+            <li>la photo et le titre d'une ou de plusieurs de ses oeuvres</li>
+        </ul>
+
+
+
+
+
+    </div>
+
+    <hr>
+    <nav>
+        <a href="../index.php">Retour</a>
+    </nav>
+
+
+</body>
+
+</html>
