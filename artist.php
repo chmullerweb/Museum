219a35b1e73f9f6dd2f269b66b99c1ffd47cc4f7
chmullerweb <ch.muller.web@gmail.com>
2020-05-17 19:50:33 +0200

    Ajout Photo Couverture + Card Portrait

--- artist.php
+++ artist.php
@@ -37,43 +37,104 @@
     </div>
 
     <div class="col6 claim white">
-        <h2>L'art en couleurs</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum officiis neque nam cumque iste laborum consectetur delectus aspernatur id ducimus, possimus laudantium impedit. Velit reprehenderit consequatur nostrum officiis in perspiciatis.</p>
+        <h2>Les artistes à Museum </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum officiis neque nam cumque iste laborum consectetur delectus aspernatur id ducimus, possimus laudantium impedit. </p>
     </div>
 
     <div class="col3 grayscale">
-        <img src="img/giulia-may-tz4dVl1Lk1o-unsplash.jpg" alt="" class="header-pix">
+        <img src="img/heshan-perera-sJPbzKg5lTA-unsplash.jpg" alt="" class="header-pix">
     </div>
     <div class="col3 grayscale">
-        <img src="img/jannis-lucas-T4t9nr18DF8-unsplash.jpg" alt="" class="header-pix">
+        <img src="img/noah-buscher-ntQ-DWsyh8o-unsplash.jpg" alt="" class="header-pix">
     </div>
     <div class="col3 grayscale">
-        <img src="img/jay-chan-T5FRGJbBEQE-unsplash.jpg" alt="" class="header-pix">
+        <img src="img/jeremy-bishop-Xf2h7jBeKIU-unsplash.jpg" alt="" class="header-pix">
     </div>
     <div class="col3 grayscale">
-        <img src="img/wengang-zhai-Ts1W-Xbt_Qk-unsplash.jpg" alt="" class="header-pix">
+        <img src="img/shttefan-Q--oPI42ArQ-unsplash.jpg" alt="" class="header-pix">
     </div>
 
-    
+  <!--  <div class="col3 row4">
+        <button class="btn-artist-name">En savoir plus</button>
+    </div>
+    <div class="col3 ">
+        <button class="btn-artist-name">En savoir plus</button>
+    </div>
+    <div class="col3 row4">
+        <button class="btn-artist-name">En savoir plus</button>
+    </div>
+    <div class="col3 row4">
+        <button class="btn-artist-name">En savoir plus</button>
+    </div>-->
+
     <div class="header-txt-bloc col12 flex bg-grey">
-        <h3 class="header-txt">un Portrait Urbain</h3>
-        <h4 class="subtitle">Exposition Permanente 2020</h4>
+        <h3 class="header-txt">Artistes en exposition</h3>
+        <h4 class="subtitle">Saison 2020</h4>
     </div>
 
-
+<!--        // ARTISTE 1 //    -->
     <div class="white artist-txt col3 mg-l30">
-        <h2 class="orange">Tokyo Luv</h2>
+        <h2 class="orange mt-0">Tokyo Luv</h2>
         <p>Artiste du mois</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sit optio quasi iste aut natus quas temporibus tempore suscipit voluptatibus aperiam modi incidunt perspiciatis, rem sint culpa dolor molestiae, dignissimos!</p>
     </div>
 
     <div class="row6 col3 btn-artist-bloc mg-l30">
-        <button>les Artistes à Museum</button>
+        <button>En savoir plus</button>
+    </div>
+    
+    <div class="col3 row5 mg-l30">
+        <img src="img/tokyo-luv-CsMNgdHXzFs-unsplash.jpg" alt="" class="artist-pix">
+    </div>
+    
+<!--        // ARTISTE 2 //    -->
+    <div class="white artist-txt col3 mg-l30">
+        <h2 class="orange mt-0">Ian Dooley</h2>
+        <p>Exposition - Juin 2020</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sit optio quasi iste aut natus quas temporibus tempore suscipit voluptatibus aperiam modi incidunt perspiciatis, rem sint culpa dolor molestiae, dignissimos!</p>
     </div>
 
-    <div class=" expo-bloc row2 col-st5 col3">
-        <p class="expo-txt">Prochaine Exposition - Juin 2020<br>
-            <span class="bold">Lumières d'été - Ian Dooley</span>
+    <div class="row6 col3 btn-artist-bloc mg-l30">
+        <button>En savoir plus</button>
+    </div>
+    
+    <div class="col3 row5 mg-l30">
+        <img src="img/ian-dooley-ZLBzMGle-nE-unsplash.jpg" alt="" class="artist-pix">
+    </div>
+    
+    <!--        // ARTISTE 3 //    -->
+    <div class="white artist-txt col3 mg-l30">
+        <h2 class="orange mt-0">Daphne Engelke</h2>
+        <p>Exposition - Juillet 2020</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sit optio quasi iste aut natus quas temporibus tempore suscipit voluptatibus aperiam modi incidunt perspiciatis, rem sint culpa dolor molestiae, dignissimos!</p>
+    </div>
+
+    <div class="row6 col3 btn-artist-bloc mg-l30">
+        <button>En savoir plus</button>
+    </div>
+    
+    <div class="col3 row5 mg-l30">
+        <img src="img/daphne-engelke-lC-ntQsuEpo-unsplash.jpg" alt="" class="artist-pix">
+    </div>
+    
+    <!--        // ARTISTE 4 //    -->
+    <div class="white artist-txt col3 mg-l30">
+        <h2 class="orange mt-0">Alex Holyoake</h2>
+        <p>Exposition - Septembre 2020</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sit optio quasi iste aut natus quas temporibus tempore suscipit voluptatibus aperiam modi incidunt perspiciatis, rem sint culpa dolor molestiae, dignissimos!</p>
+    </div>
+
+    <div class="row6 col3 btn-artist-bloc mg-l30">
+        <button>En savoir plus</button>
+    </div>
+    
+    <div class="col3 row5 mg-l30">
+        <img src="img/alex-holyoake-qX9Ie7ieb1E-unsplash.jpg" alt="" class="artist-pix">
+    </div>
+
+    <!--<div class=" expo-bloc row8 col-st5 col3">
+        <p class="expo-txt">Exposition en cours <br>
+            <span class="bold">Lumières d'été - Simone Sunrise</span>
         </p>
         <p class="expo-txt">Laborum voluptas, cum est culpa consequatur in voluptatem ipsum, saepe non nemo consectetur odio fostrum nihil quae voluptate, nobis labore fugiat excepturi. Consectetur adipisicing elit.<br><br>
             Natus consequuntur totam. Odit ipsumnihil quae voluptate, nobis dolor sit amet, consectetur adipisicing elit. Cum porro labore non laborum, dolorem voluptas, quasi aliquid quos, molestias voluptatibus consectetur. <br><br>
@@ -81,15 +142,13 @@
         <p class="expo-txt italic">
             Beatae in dicta quaerat 2020.
         </p>
-    </div>
+    </div>-->
 
-    <div class="col4 row2">
+  <!--  <div class="col4 row2">
         <img src="img/ian-dooley-ZLBzMGle-nE-unsplash.jpg" alt="" class="expo-pix">
-    </div>
+    </div>-->
 
-    <div class="col3 mg-l30">
-        <img src="img/tokyo-luv-CsMNgdHXzFs-unsplash.jpg" alt="" class="artist-pix">
-    </div>
+    
 
     <div class="musee-pix-bloc col12 pattern-dots-md orange max-w-20pc overflow-visible">
         <img src="img/scott-webb-ab80p262fFM-unsplash.jpg" alt="" class="musee-pix">
